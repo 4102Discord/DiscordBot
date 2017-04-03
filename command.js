e28d5e98fbda0aace8ec63c27f30a644755c71c5
Sparkster99 <phiggs@uncc.edu>
2017-04-03 13:35:39 -0400

    Update command.js

--- command.js
+++ command.js
@@ -53,7 +53,7 @@ var Command = new class Command {
 
             // displays all words in the blacklist
             case cmd + "showblacklist":
-                //this.blacklist.showBlacklist(message.channel);
+                this.blacklist.showBlacklist();
                 message.author.sendMessage(showBlacklist);
                 break;
         }
