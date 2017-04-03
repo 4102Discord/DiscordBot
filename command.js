4751fd70ad2f0ac729bcc1025d99b70f9b4c5472
Sparkster99 <phiggs@uncc.edu>
2017-04-03 14:42:35 -0400

    Update command.js

--- command.js
+++ command.js
@@ -17,7 +17,8 @@ var Command = new class Command {
                     cmd + 'add - add a word to the Blacklist\n' +
                     cmd + 'remove - remove a word from the Blacklist\n' +
                     cmd + 'clear - clears all words form the Blacklist\n' +
-                    cmd + 'showblacklist - displays all words in the Blacklist'
+                    cmd + 'showblacklist - displays all words in the Blacklist\n' +
+                    cmd + 'kick - kicks the user specified by the Owner or Mod'
                 );
                 break;
 
@@ -54,6 +55,20 @@ var Command = new class Command {
             // displays all words in the blacklist
             case cmd + "showblacklist":
                 this.blacklist.showBlacklist(message.channel);
+                break;
+                
+            // kicks a user specified
+                case cmd + "kick":
+        if(hasRole(message.member, "Owner") || (hasRole(message.member, "Mod"))){
+            if(words.length === 1){
+            message.channel.sendMessage('You did not define an argument Usage: !kick [message to kick a user]');
+           } else {
+                message.guild.member(message.mentions.users.first()).kick();
+            } 
+        }
+        else {
+                message.channel.sendMessage("You are not an Owner or a Mod");
+    }
                 break;
         }
     }
